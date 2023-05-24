b12f5a887261056476e455cb454b4e21435372d9
Pyry663 <100070729+Pyry663@users.noreply.github.com>
2023-05-24 19:36:32 +0000

    Now it SAVES!!

--- scripts.js
+++ scripts.js
@@ -1,11 +1,37 @@
 /* Place your JavaScript in this file */
-var parinaamt = 0;
-var lipitysmaksu = 10;
-var lipitysamt = 1;
-var edtier = 1;
-var infinityCost = 100000;
-var infinityMulti = 1;
-var lippitekst = document.getElementById("maksu")
+const listofdata = [paramt,lipamt,eti,lipco,infco,infmul]
+for (let i in listofdata) {
+    if (localStorage.getItem(i) == null) {
+        parinaamt = 0;
+        lipitysamt = 1;
+        lipitysmaksu = 10;
+        infinityCost = 10000;
+        infinityMulti = 1;
+        edtier = 1;
+    }
+    else {
+        if (i = paramt) {
+            parinaamt = localStorage.getItem(i);
+        }
+        if (i = lipamt) {
+            lipitysamt = localStorage.getItem(i);
+        }
+        if (i = lipco) {
+            lipitysmaksu = localStorage.getItem(i);
+        }
+        if (i = eti) {
+            edtier = localStorage.getItem(i);
+        }
+        if (i = infinityCost) {
+            infco = localStorage.getItem(i);
+        }
+        if (i = infinityMulti) {
+            infmul = localStorage.getItem(i);
+        }
+    }
+} 
+var savinggg = document.getElementById("saveign");
+var lippitekst = document.getElementById("maksu");
 var ed = document.getElementById("ED");
 var parinatxt = document.getElementById("parina");
 lippitekst.textContent = "Päivitä lipitys taitoja <br> Maksaa: " + lipitysmaksu;
@@ -34,6 +60,20 @@ function infinityED() {
         infinityCost = infinityCost * 2.71;
         
     }
+}
+function sav() {
+    savinggg.textContent = "";
+    setTimeout(savegame,8000);
+}
+function savegame() {
+    savinggg.textContent = "saving";
+    localStorage.setItem(paramt, parinaamt);
+    localStorage.setItem(lipamt, lipitysamt);
+    localStorage.setItem(eti, edtier);
+    localStorage.setItem(lipco, lipitysmaksu);
+    localStorage.setItem(infco, infinityCost);
+    localStorage.setItem(infmul, infinityMulti);
+    setTimeout(sav,2000);
 }
 function lipitysosto() {
     if (parinaamt >= lipitysmaksu) {
