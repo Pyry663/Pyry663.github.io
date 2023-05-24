cd308845c99abad32da80acf73d783ac56c46c9a
Pyry663 <100070729+Pyry663@users.noreply.github.com>
2023-05-24 16:09:52 +0000

    can buy multiplier

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 /* Place your JavaScript in this file */
 var parinaamt = 0;
 var lipitysmaksu = 10;
+var lipitysamt = 1;
 function clickEventimg() {
     var ed = document.getElementById("ED");
     var parinatxt = document.getElementById("parina");
     ed.style.height = "300px";
     ed.style.width = "99px";
     setTimeout(clickEventimgup,20)
-    parinaamt++;
+    parinaamt = 1 * lipitysamt;
     parinatxt.textContent = "Pärinä: " + parinaamt;
 }
 function lipitysosto() {
     if (parinaamt >= lipitysmaksu) {
         parinaamt = parinaamt - lipitysmaksu;
+        lipitysmaksu = lipitysmaksu * 2;
+        lipitysamt += 1;
+        document.getElementById("maksu").textContent = "Päivitä lipitys taitoja <br> Maksaa: " + lipitysmaksu;
     }
 }
 function clickEventimgup() {
